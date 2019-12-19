19a1a1dc0b949985ba8fb9c93d381cb0cb1572b9
nmitcheats <nmitcheats@yandex.com>
2019-12-20 02:57:05 +0530

    Incomplete but More Optimized

--- app/src/main/java/com/example/p4/MainActivity.java
+++ app/src/main/java/com/example/p4/MainActivity.java
@@ -6,15 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
